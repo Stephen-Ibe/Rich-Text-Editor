e239a0703787ccce5b04b1b30adfe48c50df1720
Stephen-Ibe <saintkalson@gmail.com>
2023-02-23 08:15:58 +0100

    modal

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,32 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useEffect, useRef, useState, Fragment } from 'react';
 import ReactQuill from 'react-quill';
 import { FaPlus } from 'react-icons/fa';
+import { AiFillPicture } from 'react-icons/ai';
+import { TiVideo } from 'react-icons/ti';
+import { RiBubbleChartFill } from 'react-icons/ri';
 import 'react-quill/dist/quill.snow.css';
+import { Popover, Transition } from '@headlessui/react';
+
+const solutions = [
+  {
+    name: 'Picture',
+    description: 'JPEG, PNG, SVG',
+    value: 'picture',
+    icon: <AiFillPicture size={28} />,
+  },
+  {
+    name: 'Video',
+    description: 'JWPlayer, YouTube, Vimeo',
+    value: 'video',
+    icon: <TiVideo size={28} />,
+  },
+  {
+    name: 'Social',
+    description: 'Instagram, Twitter, FaceBook, Snapchat, TikTok',
+    value: 'social',
+    icon: <RiBubbleChartFill size={28} />,
+  },
+] as const;
 
 function App() {
   const [content, setContent] = useState('');
@@ -31,6 +56,11 @@ function App() {
     'bullet',
   ];
 
+  const inputChange = (value: string) => {
+    setContent(value);
+    console.log(value);
+  };
+
   const editorRef = useRef<ReactQuill>(null);
 
   useEffect(() => {
@@ -50,15 +80,58 @@ function App() {
         <div>
           <ReactQuill
             value={content}
-            onChange={setContent}
+            onChange={inputChange}
             modules={modules}
             formats={formats}
             ref={editorRef}
           />
         </div>
-        <div className='add_btn'>
-          <FaPlus size={12} />
-        </div>
+        {content !== '' && (
+          <div className='add_btn'>
+            <Popover className='relative'>
+              <Popover.Button className='btn'>
+                <div>
+                  <FaPlus size={12} />
+                </div>
+              </Popover.Button>
+              <Transition
+                as={Fragment}
+                enter='transition ease-out duration-200'
+                enterFrom='opacity-0 translate-y-1'
+                enterTo='opacity-100 translate-y-0'
+                leave='transition ease-in duration-150'
+                leaveFrom='opacity-100 translate-y-0'
+                leaveTo='opacity-0 translate-y-1'
+              >
+                <Popover.Panel className='absolute left-48 z-10 mt-3 w-screen max-w-sm -translate-x-1/2 transform px-4 sm:px-0 lg:max-w-3xl'>
+                  <div className='overflow-hidden rounded-lg shadow-lg ring-1 ring-black ring-opacity-5'>
+                    <div className='bg-gray-50 p-4'>
+                      <h3 className='text-sm'>Embeds</h3>
+                    </div>
+                    <div className='relative grid bg-white lg:grid-cols-2'>
+                      {solutions.map(({ icon, value, name, description }) => (
+                        <div
+                          key={name}
+                          className='flex items-start rounded-lg transition duration-150 ease-in-out hover:bg-gray-50 focus:outline-none focus-visible:ring focus-visible:ring-orange-500 focus-visible:ring-opacity-50 p-4 '
+                        >
+                          <div className=''>{icon}</div>
+                          <div className='ml-4'>
+                            <p className='text-sm font-medium text-gray-900'>
+                              {name}
+                            </p>
+                            <p className='text-xs text-gray-400'>
+                              {description}
+                            </p>
+                          </div>
+                        </div>
+                      ))}
+                    </div>
+                  </div>
+                </Popover.Panel>
+              </Transition>
+            </Popover>
+          </div>
+        )}
       </div>
     </div>
   );

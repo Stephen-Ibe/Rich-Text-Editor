d271e47ef777a6cfbeb3cb6a55c57676b3437495
Stephen-Ibe <saintkalson@gmail.com>
2023-02-23 06:38:38 +0100

    editor

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,53 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import ReactQuill from 'react-quill';
+import 'react-quill/dist/quill.snow.css';
 
 function App() {
+  const [content, setContent] = useState('');
+
+  const modules = {
+    toolbar: [
+      [{ header: [1, 2, 3, 4, 5, 6, false] }],
+      ['link', 'image'],
+      [{ align: [] }],
+      ['bold', 'italic', 'underline', 'strike', 'blockquote'],
+      [{ list: 'ordered' }, { list: 'bullet' }],
+      ['clean'],
+    ],
+  };
+
+  const formats = [
+    'header',
+    'link',
+    'image',
+    'align',
+    'bold',
+    'italic',
+    'underline',
+    'strike',
+    'blockquote',
+    'list',
+    'bullet',
+  ];
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container mx-auto'>
+      <div className='my-4'>
+        <input
+          type='text'
+          placeholder='This is the title'
+          className='my-4 placeholder:font-semibold text-black placeholder-black placeholder:text-xl focus:outline-none font-semibold text-xl border w-full py-2 placeholder:tracking-wider	'
+        />
+      </div>
+      <div>
+        <ReactQuill
+          value={content}
+          onChange={setContent}
+          modules={modules}
+          formats={formats}
+          className='border-0 h-[80%]'
+        />
+      </div>
     </div>
   );
 }

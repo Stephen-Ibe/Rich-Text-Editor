cb07cfc752e85c0c05918f8c177df4cd0b7363dd
Stephen-Ibe <saintkalson@gmail.com>
2023-02-23 07:01:56 +0100

    editor UI  updates

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
-import React, { useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import ReactQuill from 'react-quill';
+import { FaPlus } from 'react-icons/fa';
 import 'react-quill/dist/quill.snow.css';
 
 function App() {
@@ -30,23 +31,35 @@ function App() {
     'bullet',
   ];
 
+  const editorRef = useRef<ReactQuill>(null);
+
+  useEffect(() => {
+    editorRef?.current?.focus();
+  }, []);
+
   return (
-    <div className='container mx-auto'>
+    <div className='container mx-auto px-4'>
       <div className='my-4'>
         <input
           type='text'
           placeholder='This is the title'
-          className='my-4 placeholder:font-semibold text-black placeholder-black placeholder:text-xl focus:outline-none font-semibold text-xl border w-full py-2 placeholder:tracking-wider	'
+          className='placeholder:font-semibold text-black placeholder-black placeholder:text-xl focus:outline-none font-semibold text-xl w-full py-2 placeholder:tracking-wider	'
         />
       </div>
-      <div>
-        <ReactQuill
-          value={content}
-          onChange={setContent}
-          modules={modules}
-          formats={formats}
-          className='border-0 h-[80%]'
-        />
+      <div className='relative max-h-full'>
+        <div>
+          <ReactQuill
+            value={content}
+            onChange={setContent}
+            modules={modules}
+            formats={formats}
+            ref={editorRef}
+            className=''
+          />
+        </div>
+        <div className='add_btn'>
+          <FaPlus size={12} />
+        </div>
       </div>
     </div>
   );

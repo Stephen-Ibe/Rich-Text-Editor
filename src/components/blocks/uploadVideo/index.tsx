db8ce4a61dd549a21a698403fd1336104d00176f
Stephen-Ibe <saintkalson@gmail.com>
2023-02-25 09:12:38 +0100

    video upload component

--- src/components/blocks/uploadVideo/index.tsx
+++ src/components/blocks/uploadVideo/index.tsx
@@ -1,17 +1,40 @@
-import React from 'react';
+import React, { FormEvent, useState } from 'react';
 import { Modal } from '../../common';
 
 type Props = { closeModal(): void };
 
+const providers = [
+  { id: 0, name: 'YouTube', value: 'youtube' },
+  { id: 1, name: 'Vimeo', value: 'vimeo' },
+] as const;
+
 const UploadVideo = ({ closeModal }: Props) => {
+  const [userChoice, setUserChoice] = useState<string>('');
+
+  const handleChange = (e: FormEvent<HTMLSelectElement>) => {
+    setUserChoice(e.currentTarget.value);
+  };
+
   return (
     <Modal isOpen closeModal={closeModal}>
       <div className='my-4'>
         <div className='my-4'>
-          <p className='font-semibold'>Upload Image</p>
-          <select>
-            <option>YouTube</option>
+          <label className='text-xs uppercase'>Video Provider</label>
+          <select
+            className='w-full border py-2 px-1 outline-none mt-1'
+            onChange={handleChange}
+            value={userChoice}
+          >
+            {providers.map(({ id, name, value }) => (
+              <option key={id} value={value}>
+                {name}
+              </option>
+            ))}
           </select>
+        </div>
+        <div className='my-4 flex flex-col gap-y-1'>
+          <label className='text-xs uppercase'>url</label>
+          <input type='text' className='border px-1 py-2 outline-none' />
         </div>
       </div>
     </Modal>
